e083add239cffff46d0c8c5dff45057f4c4f367d
JackWils91 <jackdavidwilson@gmail.com>
2019-07-01 14:47:15 +0100

    new components for article and comments added to clean up articlePage and also for additional Material UI design

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -3,6 +3,14 @@ import * as api from "./api";
 import CommentPost from "./CommentPost";
 import Error from "./Error";
 import Voter from "./Voter";
+import Button from "@material-ui/core/Button";
+
+import ArticlePageArticle from "./ArticlePageArticle";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+
+import { TextField } from "@material-ui/core";
+import ArticlePageComments from "./ArticlePageComments";
 
 class ArticlePage extends Component {
   state = {
@@ -57,44 +65,31 @@ class ArticlePage extends Component {
     if (error) return <Error error={error} />;
 
     return (
-      <>
-        <p>{author}</p>
-        <p>{title}</p>
-        <p>{body}</p>
-        <Voter votes={votes} id={article_id} type="article" />
-        <div />
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm md">
+          <ArticlePageArticle author={author} title={title} body={body} />
+          <Voter votes={votes} id={article_id} type="article" />
+          {/* <p>Comments={comment_count}</p> */}
 
-        <div />
-        <form onSubmit={this.handleSubmit}>
-          <label>Comment: </label>{" "}
-          <input
-            onChange={this.handleChange}
-            name={"postComment"}
-            value={postComment}
-          />
-          <button>Submit</button>
-        </form>
-        <CommentPost />
-        {comments.map(({ author, created_at, body, votes, comment_id }) => (
-          <React.Fragment key={comment_id}>
-            <p>
-              {created_at} {author} {votes}
-            </p>
-            <p>{body}</p>
-            {username === author && (
-              <button onClick={() => this.deleteComment(comment_id)}>
-                Delete!
-              </button>
-            )}
-
-            {username !== author && (
-              <Voter votes={votes} id={comment_id} type="comment" />
-            )}
-          </React.Fragment>
-        ))}
-
-        {/* <p>Comments={comment_count}</p> */}
-      </>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              onChange={this.handleChange}
+              name={"postComment"}
+              value={postComment}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+          <CommentPost />
+          {comments.map(comment => (
+            <ArticlePageComments
+              {...comment}
+              username={username}
+              deleteComment={this.deleteComment}
+            />
+          ))}
+        </Container>
+      </React.Fragment>
     );
   }
 
